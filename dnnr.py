2544dd0c30da063b208dd1c59969e07f1094155e
何为 <49465847+innovation1998@users.noreply.github.com>
2021-07-04 17:48:06 +0800

    Update dnnr.py
    add a new parameter "shortcut_num"  to set shortcut connections.

--- dnnr.py
+++ dnnr.py
@@ -7,11 +7,12 @@ from deepctr_torch.layers.activation import activation_layer
 
 class DNNR(nn.Module):
     def __init__(self, inputs_dim, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=True,
-                 init_std=0.0001, dice_dim=2, seed=1024, device='cpu'):
+                 init_std=0.0001, dice_dim=2,shortcut_num=2, seed=1024, device='cpu'):
         super(DNNR, self).__init__()
         self.dropout_rate = dropout_rate
         self.dropout = nn.Dropout(dropout_rate)
         self.seed = seed
+        self.shorcut_num = shorcut_num
         self.l2_reg = l2_reg
         self.use_bn = use_bn
         if len(hidden_units) == 0:
@@ -26,6 +27,9 @@ class DNNR(nn.Module):
                 [nn.BatchNorm1d(hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
 
         self.shortcut = nn.Linear(hidden_units[0], hidden_units[1], bias=False)
+
+        if self.shorcut_num > len(self.linears):
+            raise ValueError('Improper shortcut number! The shortcut number should not greater than hidden layer depth.')
 
         self.activation_layers = nn.ModuleList(
             [activation_layer(activation, hidden_units[i + 1], dice_dim) for i in range(len(hidden_units) - 1)])
@@ -42,9 +46,10 @@ class DNNR(nn.Module):
         for i in range(len(self.linears)):
             
             fc = self.linears[i](deep_input)
-            if i > 0:
+
+            if i > 0 & i < self.shorcut_num:
                 fc += deep_input
-            else:
+            elif i == 0 & self.shorcut_num > 0:
                 fc += project_x
 
             if self.use_bn:
